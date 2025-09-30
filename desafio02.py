687ecddb9171303e22f624a7639f481a16c406a6
Filipe S. Amoedo <filipesamoedo@gmail.com>
2025-09-30 15:18:11 -0300

    Create desafio02.py

---
+++ desafio02.py
@@ -0,0 +1,7 @@
+""" Crie um script Python que leia o dia, o mês e o ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada """
+""" O usuário entra com o dia, mes e ano separatamente, o programa salva nas variáveis e apresenta no terminal tudo junto e com a formatação padrão de data """
+
+dia = input('Qual seu dia de nascimento?')
+mes = input('Qual seu mes de nascimento?')
+ano = input('Qual seu ano de nascimento?')
+print('Você nasceu dia', dia,'/',mes,'/',ano)
