0808aafe87c6c2336047f58ab9f7db21fa1fa845
Filipe S. Amoedo <filipesamoedo@gmail.com>
2025-09-30 15:11:40 -0300

    Create desafio01.py

---
+++ desafio01.py
@@ -0,0 +1,5 @@
+""" Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado """
+""" O usuário responde a pergunta, o programa registra o que foi digitado e devolve com a mensagem print """
+
+nome = input('Qual o seu nome?')
+print('Seja muito bem-vindo,', nome,', ao infinito universo de Python!')
