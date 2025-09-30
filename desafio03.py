c241fff44d8c641be0a0c9c0f49eac6d597b96ab
Filipe S. Amoedo <filipesamoedo@gmail.com>
2025-09-30 20:05:01 -0300

    Create desafio03.py

---
+++ desafio03.py
@@ -0,0 +1,19 @@
+# Desafio 3
+# Crie um script Python que leia dois números e teste mostrar a soma entre eles.
+
+# Solicitação ao usuário dos dois números
+
+num1 = float(input('Digite o primeiro número:'))
+num2 = float(input('Digite o segundo número:'))
+
+# Impressão com cálculo
+
+print('A soma entre os dois números é:', num1 + num2)
+
+# Avançando com os operadores matemáticos
+
+print('A subtração entre os dois números é:', num1 - num2)
+
+print('A multiplicação entre os dois números é:', num1 * num2)
+
+print('A divisão entre os dois números é:', num1 / num2)
