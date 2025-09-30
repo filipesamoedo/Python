ae88e74ee68a33790b07f380abf2637c7ba44fa0
Filipe S. Amoedo <filipesamoedo@gmail.com>
2025-09-30 17:23:11 -0300

    Create tipagemdinamica.py

---
+++ tipagemdinamica.py
@@ -0,0 +1,11 @@
+""" Primeiro programa envolvendo tipagem dinâmica """
+
+# Exemplo de tipagem dinâmica em Python
+# Variável 'x' inicialmente recebe um valor inteiro
+x = 10
+print("Tipo de x", type(x))
+
+# A mesma variável 'x' agora recebe uma string
+
+x = "Olá, Mundo!"
+print("Tipo de x:", type(x))
