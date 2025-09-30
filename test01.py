1616968380ef83c01f290a50c001e5904bd042e8
Filipe S. Amoedo <filipesamoedo@gmail.com>
2025-09-30 15:05:10 -0300

    Create test01.py

---
+++ test01.py
@@ -0,0 +1,9 @@
+""" Primeiro programa em Python: Interação com usuário. Inserção de valores em variáveis e devolução de impressão. """
+
+nome = input('Qual seu nome?')
+
+idade = input('Qual sua idade?')
+
+peso = input('Qual é seu peso?')
+
+print(nome,idade,peso)
