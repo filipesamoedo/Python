a92a9675e22685fc449209c823ce1219947f0341
Filipe S. Amoedo <filipesamoedo@gmail.com>
2025-09-30 17:19:39 -0300

    Create deffuncao01.py

---
+++ deffuncao01.py
@@ -0,0 +1,49 @@
+""" Definindo as primeiras função. Execício prático. """
+
+# Exemplo de código Python com sintaxe simples e legível
+# Definindo uma função que calcula a soma de dois números
+def soma(a,b):
+        return a + b
+
+# Usando a função para somar dois números
+resultado = soma(3,5)
+
+# Exibindo o resultado
+print("A soma é:", resultado)
+
+# Avançando o exercício: Criando mais funções
+
+def subtracao(a,b):
+        return a - b
+
+resultado = subtracao(3,5)
+
+print("A subtraçao é:", resultado)
+
+def multiplicacao(a,b):
+        return a * b
+
+resultado = multiplicacao(3,5)
+
+print("A multiplicação é:", resultado)
+
+def divisao(a,b):
+        return a / b
+
+resultado = divisao(3,5)
+
+print("A divisão é:", resultado)
+
+def divisaointeira(a,b):
+        return a // a
+
+resultado = divisaointeira(3,5)
+
+print("A divisão inteira é:", resultado)
+
+def restodadivisao(a,b):
+        return a % b
+
+resultado = restodadivisao(3,5)
+
+print("O resto da divisão é:", resultado)
