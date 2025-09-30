0e72dbe5e2a62ca301e81ccccf526d70e8e75b57
Filipe S. Amoedo <filipesamoedo@gmail.com>
2025-09-30 17:29:49 -0300

    Create biblioteca01.py

---
+++ biblioteca01.py
@@ -0,0 +1,35 @@
+# Exemplo de uso da biblioteca padrão do Python (módulo math)
+import math
+
+# Calculando a raiz quadrada de um número
+numero = 16
+raiz_quadrada = math.sqrt(numero)
+
+print("A raiz quadrada de", numero, "é", raiz_quadrada)
+
+# Avançando nos testes
+
+numero = 25
+raiz_quadrada = math.sqrt(numero)
+
+print("A raiz quadrada de", numero, "é", raiz_quadrada)
+
+numero = 36
+raiz_quadrada = math.sqrt(numero)
+
+print("A raiz quadrada de", numero, "é", raiz_quadrada)
+
+numero = 81
+raiz_quadrada = math.sqrt(numero)
+
+print("A raiz quadrada de", numero, "é", raiz_quadrada)
+
+numero = 100
+raiz_quadrada = math.sqrt(numero)
+
+print("A raiz quadrada de", numero, "é", raiz_quadrada)
+
+numero = 10000
+raiz_quadrada = math.sqrt(numero)
+
+print("A raiz quadrada de", numero, "é", raiz_quadrada)
